19b569babcf990a48d554b95bc0b8f3b17d10de7
Thanh Huy Tu <tuthanhhuy2004@gmail.com>
2025-01-10 19:43:22 -0800

    Integrate React Native Paper component library

--- software/blind-vision-software/app/index.tsx
+++ software/blind-vision-software/app/index.tsx
@@ -1,15 +1,21 @@
 import { Text, View } from "react-native";
+import * as React from 'react';
+import { PaperProvider } from 'react-native-paper';
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-500 text-2xl font-bold">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <PaperProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text className="text-red-500 text-2xl font-bold">Edit app/index.tsx to edit this screen.</Text>
+      </View>
+      <ActivityIndicator animating={true} color={MD2Colors.red800} />
+    </PaperProvider>
   );
 }
