ff43d669f6439c5ed543d6a4cbfb9152d4b66044
ucijakeh2 <jakeh2@uci.edu>
2025-01-13 20:02:30 -0800

    Renamed arduino project

--- firmware/src/bv-glasses-project/bv-glasses-project.ino
+++ firmware/src/bv-glasses-project/bv-glasses-project.ino
@@ -1,10 +1,13 @@
 #include "header.h"
 
+#define VIBRATOR_PIN 17
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   sleep(2);
   pinMode(24, OUTPUT);
+  pinMode(VIBRATOR_PIN, OUTPUT); // vibrator
   sleep(1);
 }
 
@@ -12,8 +15,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   // Serial.println("test123");
   // doPrint();
+  analogWrite(VIBRATOR_PIN, 0);
   digitalWrite(24, HIGH);
-  sleep(1);
+  sleep(3);
   digitalWrite(24, LOW);
+  
   sleep(1);
 }
