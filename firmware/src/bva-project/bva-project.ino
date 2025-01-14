a0d0cd6b134a1a1596e2282b7bd0f59fe1f382bb
user <user@desktop.lan>
2025-01-13 18:39:40 -0800

    Added src code testing manual blinking of external LED on breadboard

--- firmware/src/bva-project/bva-project.ino
+++ firmware/src/bva-project/bva-project.ino
@@ -4,10 +4,16 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   sleep(2);
+  pinMode(24, OUTPUT);
+  sleep(1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // Serial.println("test123");
-  doPrint();
+  // doPrint();
+  digitalWrite(24, HIGH);
+  sleep(1);
+  digitalWrite(24, LOW);
+  sleep(1);
 }
