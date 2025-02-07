5d512e9b29c8a0022897118d24aa7df74ce6d94b
Thanh Huy Tu <tuthanhhuy2004@gmail.com>
2025-02-06 20:34:16 -0800

    Refactor project: debug PATH too long (4.5 hours)

--- software/app/(tabs)/home.tsx
+++ software/app/(tabs)/home.tsx
@@ -8,9 +8,10 @@ import backgrounds from "@/constants/backgrounds";
 import images from "@/constants/images";
 import styles from "@/constants/styles";
 
+const SERVICE_UUID = "12345678-1234-1234-1234-123456789abc";
+const CHARACTERISTIC_UUID = "abcd1234-ab12-cd34-ef56-1234567890ab";
 
 export default function Home() {
-    
     return (
         <SafeAreaView className={styles.tabs.safeAreaViewStyle}>
             <Image 
