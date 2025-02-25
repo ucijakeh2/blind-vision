c21501fcf5d44aa9799241c93142b4da35ef44aa
Thanh Huy Tu <tuthanhhuy2004@gmail.com>
2025-02-25 15:05:25 -0800

    Fix scan and connect

--- software/app/(tabs)/home.tsx
+++ software/app/(tabs)/home.tsx
@@ -1,7 +1,6 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SystemBars } from 'react-native-edge-to-edge';
-import BleManager, { Peripheral } from 'react-native-ble-manager';
 import { Button } from "react-native-paper";
 import { Image } from "react-native";
 
@@ -16,8 +15,6 @@ import styles from "@/constants/styles";
 import ble, { SERVICE_UUID_GLASSES, SERVICE_UUID_STICK } from "@/scripts/ble";
 
 import { AuthContext, ThemeContext } from '../_layout';
-import { GlassesConnectionContext, StickConnectionContext } from "./_layout";
-
 const BLEButton: React.FC<{
     isLoading: boolean,
     trigger: () => void 
@@ -43,54 +40,22 @@ const BLEButton: React.FC<{
 }
 
 export default function Home() {
-    const [glasses, setGlasses] = useState<Peripheral | null>(null);
-    const [stick, setStick] = useState<Peripheral | null>(null);
     const [message, setSnackBarMessage] = useState<string>("");
-    const [isScanning, setScanning] = useState<boolean>(false);
     const [visible, setVisibility] = useState<boolean>(false);
 
-    const [isGlassesConnected, setGlassesConnection] = useContext(GlassesConnectionContext);
-    const [isStickConnected, setStickConnection] = useContext(StickConnectionContext);
     const [dark, _1] = useContext(ThemeContext);
     const [auth, _2] = useContext(AuthContext);
 
-    const updatedGlasses = useRef<Peripheral | null>(null);
-    const updatedStick = useRef<Peripheral | null>(null);
-
+    const triggerError = (message: string) => {
+        setSnackBarMessage(message);
+        setVisibility(true);
+    }
 
     useEffect(() => {
         ble.requestPermissions();
         ble.enableBLE();
         ble.start();
     }, []);
-
-    useEffect(() => { updatedGlasses.current = glasses }, [glasses])
-    useEffect(() => { updatedStick.current = stick }, [stick])
-
-    useEffect(() => {
-        // Listen to when ESP32 disconnect
-        const onStopDisconnectListener = BleManager.onDisconnectPeripheral((peripheral: any) => { 
-            const glasses = updatedGlasses.current;
-            const stick = updatedStick.current;
-
-            if (glasses) {
-                if (glasses.id === peripheral.peripheral) {
-                    console.log("Blind Vision Glasses disconnected");
-                    setGlassesConnection(false); return;
-                }
-            }
-
-            if (stick) {
-                console.log(stick.id, peripheral.peripheral)
-                if (stick.id === peripheral.peripheral) {
-                    console.log("Blind Vision Stick disconnected");
-                    setStickConnection(false); return;
-                }
-            }
-        })
-
-        return () => { onStopDisconnectListener.remove(); }
-    }, [])
 
     return (
         <>
@@ -99,70 +64,34 @@ export default function Home() {
                 className={styles.tabs.safeAreaViewStyle}
                 style={{backgroundColor: dark ? "#222222" : "white"}}
             >
-                {!(glasses || stick) ?
-                    <>
-                        <ThemedSnackbar
-                            message={message}
-                            isError={true}
-                            visible={visible}
-                            setVisibility={setVisibility}
-                        />
-                        <BLEButton 
-                            trigger={async () => {
-                                // Only scan & connect if BLE is enabled
-                                // Ask for it otherwise
-                                if(await ble.enableBLE()) {
-                                    setScanning(true)
-                                    const connectedDevices: Peripheral[] | null = await ble.scan();
-                                    console.log("Returned connected device: ", connectedDevices);
-
-                                    if (!connectedDevices) {
-                                        setVisibility(true);
-                                        setSnackBarMessage("Cannot find devices.");
-                                    }
-
-                                    const filteredGlasses = connectedDevices?.filter((value) => value.advertising.serviceUUIDs?.[0] === SERVICE_UUID_GLASSES)[0]
-                                    const filteredStick = connectedDevices?.filter((value) => value.advertising.serviceUUIDs?.[0] === SERVICE_UUID_STICK)[0]
-                                    console.log(filteredGlasses, filteredStick)
-                                    
-                                    if (filteredGlasses) setGlasses(filteredGlasses);
-                                    if (filteredStick) setStick(filteredStick);
-
-                                    setScanning(false);
-                                }
-                            }} 
-                            isLoading={isScanning}
-                        />
-                    </> :
-                    <>
-                        <Image 
-                            className={`${styles.tabs.headerImageStyle} -my-5`} 
-                            source={dark ? backgrounds.darkHome : backgrounds.home}
-                        />
-                        <HomeLabel nickname={auth ? auth.value : "null"}/>
-                        <CustomDeviceCard
-                            deviceName="Glasses"
-                            sliderName="Glasses volume"
-                            destination="/settings/glasses"
-                            connected={isGlassesConnected}
-                            setConnection={setGlassesConnection}
-                            imageSource={images.glasses}
-                            bleId={glasses ? glasses.id : ""}
-                            serviceUUID={SERVICE_UUID_GLASSES}
-                        />
-                        <CustomDeviceCard
-                            deviceName="Stick"
-                            sliderName="Blind stick vibration"
-                            destination="/settings/stick"
-                            connected={isStickConnected}
-                            setConnection={setStickConnection}
-                            imageSource={images.blindStick}
-                            bleId={stick ? stick.id : ""}
-                            serviceUUID={SERVICE_UUID_STICK}
-                        /> 
-                    </>
-                }
+                <Image 
+                    className={`${styles.tabs.headerImageStyle} -my-5`} 
+                    source={dark ? backgrounds.darkHome : backgrounds.home}
+                />
+                <HomeLabel nickname={auth ? auth.value : "null"}/>
+                <CustomDeviceCard
+                    deviceName="Glasses"
+                    sliderName="Glasses volume"
+                    destination="/settings/glasses"
+                    imageSource={images.glasses}
+                    serviceUUID={SERVICE_UUID_GLASSES}
+                    setErrorMessage={triggerError}
+                />
+                <CustomDeviceCard
+                    deviceName="Stick"
+                    sliderName="Blind stick vibration"
+                    destination="/settings/stick"
+                    imageSource={images.blindStick}
+                    serviceUUID={SERVICE_UUID_STICK}
+                    setErrorMessage={triggerError}
+                /> 
             </SafeAreaView>
+            <ThemedSnackbar
+                message={message}
+                isError={true}
+                visible={visible}
+                setVisibility={setVisibility}
+            />
         </>
     )
 }
--- software/components/tabs/home/CustomDeviceCard.tsx
+++ software/components/tabs/home/CustomDeviceCard.tsx
@@ -1,13 +1,17 @@
-import { Dispatch, SetStateAction, useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 import { Link, type LinkProps } from 'expo-router'
+import BleManager from 'react-native-ble-manager'
 import { Button } from 'react-native-paper'
 import { View, Image } from 'react-native'
 
 import CustomStatusBanner from './CustomStatusBanner'
 import CustomSlider from './CustomSlider'
-import ble from '@/scripts/ble'
+
+import { GlassesConnectionContext, StickConnectionContext } from '@/app/(tabs)/_layout'
 import { ThemeContext } from '@/app/_layout'
+
+import ble, { SERVICE_UUID_GLASSES } from '@/scripts/ble'
 
 const MAX_VALUE = 255;
 const STEP = 10;
@@ -17,39 +21,69 @@ interface CustomDeviceCardProps {
     deviceName: string,
     sliderName: string,
     destination: LinkProps["href"],
-    connected: boolean,
-    setConnection: Dispatch<SetStateAction<boolean>>,
     imageSource: object,
-    bleId: string,
-    serviceUUID: string
+    serviceUUID: string,
+    setErrorMessage: (message: string) => void,
 }
 
 const CustomDeviceCard: React.FC<CustomDeviceCardProps> = ({
     deviceName,
     sliderName,
     destination,
-    connected,
-    setConnection,
     imageSource,
-    bleId,
-    serviceUUID
+    serviceUUID,
+    setErrorMessage
 }) => {
-  const [value, setValue] = useState(-1);
+  const [bleId, setBleId] = useState<string>("");
+  const [value, setValue] = useState<number>(-1);
   const [dark, _] = useContext(ThemeContext);
+  const [isDeviceConnected, setDeviceConnection] = (serviceUUID === SERVICE_UUID_GLASSES) ? 
+                                                   useContext(GlassesConnectionContext) :
+                                                   useContext(StickConnectionContext)
+
+  const updatedDeviceId = useRef<string | null>(null);
 
   const valueToByte = (value: number) => (Math.round((value / STEP) * UNIT));
   const byteToValue = (byte: any) => (Math.round(byte / UNIT) * STEP);
-  const handleConnection = async () => { 
-    const bleConnected = await ble.connect(bleId);
-    if (bleConnected) {
-      setConnection(bleConnected); 
-      const configBytes = await ble.read(bleId, serviceUUID);
-      if (configBytes && configBytes.length === 1) { setValue(byteToValue(configBytes[0])); }
-      else { setValue(0); }
-    }
+
+  const handleConnection = async () => {
+    const deviceName = (serviceUUID === SERVICE_UUID_GLASSES) ? "Glasses" : "Stick"
+    const scannedDeviceId = await ble.scan(serviceUUID)
+
+    if (scannedDeviceId) {
+      const bleConnected = await ble.connect(scannedDeviceId);
+      
+      if (bleConnected) {
+        setBleId(scannedDeviceId);
+        setDeviceConnection(bleConnected); 
+
+        const configBytes = await ble.read(scannedDeviceId, serviceUUID);
+        if (configBytes && configBytes.length === 1) { setValue(byteToValue(configBytes[0])); }
+        else { setValue(0); }
+      }
+      else { setErrorMessage(`${deviceName} found but cannot be connected`) }
+    } else { setErrorMessage(`${deviceName} cannot be found`) }
   }
 
-  useEffect(() => { if (value > 0) ble.write(bleId, serviceUUID, [valueToByte(value)]); }, [value])
+  useEffect(() => { if (value >= 0) ble.write(bleId, serviceUUID, [valueToByte(value)]); }, [value])
+  useEffect(() => { updatedDeviceId.current = bleId }, [bleId])
+
+  useEffect(() => {
+    // Listen to when ESP32 disconnect
+    const onStopDisconnectListener = BleManager.onDisconnectPeripheral((peripheral: any) => { 
+        const deviceId = updatedDeviceId.current;
+
+        if (deviceId && deviceId === peripheral.peripheral) {
+            const deviceName = (serviceUUID === SERVICE_UUID_GLASSES) ? "Glasses" : "Stick"
+            console.log(`Blind Vision ${deviceName} disconnected`);
+            setErrorMessage(`Blind Vision ${deviceName} disconnected`);
+            setDeviceConnection(false);
+          }
+        }
+    )
+
+    return () => { onStopDisconnectListener.remove(); }
+  }, [])
 
   const theme = (connected: boolean, dark: boolean) => {
     if (!connected) {
@@ -87,26 +121,26 @@ const CustomDeviceCard: React.FC<CustomDeviceCardProps> = ({
     <View className='shadow shadow-neutral-400 h-2/5 w-11/12 rounded-3xl'>
       <LinearGradient
         className='h-full rounded-3xl flex flex-row items-center justify-start p-6'
-        colors={[theme(connected, dark).gradientFrom, theme(connected, dark).gradientTo]} 
+        colors={[theme(isDeviceConnected, dark).gradientFrom, theme(isDeviceConnected, dark).gradientTo]} 
         start={{ x: 0, y: 0 }}
         end={{ x: 0, y: 1 }}
       >
-        {connected ?
+        {isDeviceConnected ?
           <CustomSlider sliderName={sliderName} step={STEP} value={value} setValue={setValue} disabled={false}/>:
           <><CustomSlider sliderName={sliderName} step={STEP} value={0} setValue={setValue} disabled={true}/></>
         }
         <Image className='absolute -right-4 top-1/4 scale-90' source={imageSource}/>
         <View className='h-full flex-1 flex flex-col items-end justify-between ml-6'>
-          <CustomStatusBanner connected={connected}/>
-          <Link href={connected ? destination : "/(tabs)/home"} withAnchor asChild>
+          <CustomStatusBanner connected={isDeviceConnected}/>
+          <Link href={isDeviceConnected ? destination : "/(tabs)/home"} withAnchor asChild>
             <Button
               className="w-full"
               mode="outlined"
-              textColor={theme(connected, dark).buttonColor}
-              theme={{colors: { outline: theme(connected, dark).buttonColor }}}
-              onPress={handleConnection}
+              textColor={theme(isDeviceConnected, dark).buttonColor}
+              theme={{colors: { outline: theme(isDeviceConnected, dark).buttonColor }}}
+              onPress={ !isDeviceConnected ? handleConnection : () => {} }
               >
-                {connected ? `${deviceName} Settings` : `Connect ${deviceName}`}
+                {isDeviceConnected ? `${deviceName} Settings` : `Connect ${deviceName}`}
             </Button>
           </Link>
         </View>
